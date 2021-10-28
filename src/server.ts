179253f476a20364d08701c07ee95fd4209ba3f7
renemonteiro <rene.m.santos@accenture.com>
2021-10-27 22:02:46 -0300

    atualização

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).send({ message: "olá mundo" });
+  res.status(200).send({ message: "olá mundooo" });
 });
 
 app.listen(process.env.PORT || 3003, () => {
