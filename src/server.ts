b21ac9db39808cef5ab35d5bcf515b81e7acf830
renemonteiro <rene.m.santos@accenture.com>
2022-04-24 08:43:01 -0300

    exclusão de query e pasta idea

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
-import { connection } from "./database/connection";
 
 const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  const a = async () => {
-    await connection.schema.raw(`
-      INSERT INTO teste VALUES(
-        10,
-        "teste2"
-      );
-    `);
-  };
-  a();
+
   res.status(200).send({ message: "olá mundo!!" });
 });
 
